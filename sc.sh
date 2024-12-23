9977110c420b562eda668b140c6745c1fadbe7cd
kir450 <163303240+kir450@users.noreply.github.com>
2024-12-23 12:52:14 +0700

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Системная переменная с текущей датой
+data=$(date +%d.%m.%Y-%H:%M:%S)
+
+# Создание директории с текущей датой/временем
+mkdir /var/backup/$data
+
+# Копирование настроек frr
+cp -r /etc/frr /var/backup/$data
+
+# Копирование настроек nftables
+cp -r /etc/nftables /var/backup/$data
+
+# Копирование настроек сетевых интерфейсов
+cp -r /etc/NetworkManager/system-connections /var/backup/$data
+
+#Копирование настроек DHCP
+cp -r /etc/dhcp /var/backup/$data
+
+#Переход в директорию
+cd /var/backup
+
+cpr /etc/dhcp /var/backup/$data
+
+# Архивируем
+tar czfv "./$data.tar.gz" ./$data
+
+# Удаляем временную директорию
+rm -r /var/backup/$data
